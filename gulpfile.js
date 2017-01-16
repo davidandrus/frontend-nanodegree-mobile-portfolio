2babc349aee5d970f9a1f02c0c2d960360c4ac7e
davidandrus <davidandrus.com@gmail.com>
2017-01-15 22:22:03 -0800

    fix: getting build task running correctly

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,37 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
+const gulpIf = require('gulp-if');
+const inlineCSS = require('gulp-inline-css');
+const cheerio = require('gulp-cheerio');
 
 gulp.task('copy', () => {
   // copy files
   gulp
-    .src(['src/*', 'src/*/**', '!src/img', '!src/views/images'])
+    .src([
+      'src/*',
+      'src/*/**',
+      '!src/**/*.{jpg,gif,png,svg}',
+      '!src/**/*.html',
+      '!src/views/images',
+      '!src/**/*.css'
+    ])
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify', ['copy'], () => {
+gulp.task('build', ['copy'], () => {
   gulp
-    .src(['src/**/*.jpg','src/**/*.gif', 'src/**/*.png', 'src/**/*.svg'])
+    .src('src/**/*.{jpg,gif,png,svg}')
     .pipe(imagemin())
     .pipe(gulp.dest('dist'));
 
   gulp
     .src('src/**/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulpIf(['index.html'], inlineCSS({ removeLinkTags: false })))
+    .pipe(gulpIf(['index.html'], cheerio(function($) {
+      $('link[media="screen"]').remove();
+    })))
     .pipe(gulp.dest('dist'));
 
   gulp
@@ -27,4 +41,4 @@ gulp.task('minify', ['copy'], () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['copy', 'minify']);
+gulp.task('default', ['build']);
