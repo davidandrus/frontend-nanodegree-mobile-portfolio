bb37d515e560a672ad0802a4ba28eb234359fc9a
davidandrus <davidandrus.com@gmail.com>
2017-01-15 17:55:48 -0800

    feat: adding gulp imagemin task

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('default', () => {
+  gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
