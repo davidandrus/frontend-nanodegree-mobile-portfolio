912084e16815878b2a75f4f126eeae0ebf322900
davidandrus <davidandrus.com@gmail.com>
2017-01-15 18:49:45 -0800

    feat: adding gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,20 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('default', () => {
-  gulp.src('src/img/*')
+  // copy files
+  gulp
+    .src(['src/*', 'src/*/**', '!src/img'])
+    .pipe(gulp.dest('dist'));
+
+  gulp
+    .src('src/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
+
+  gulp
+    .src('src/views/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/views/images'));
+
+
 });
