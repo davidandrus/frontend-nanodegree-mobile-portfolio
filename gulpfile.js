1267686ae85e9bb7aa86a64540454d11e755e5d8
davidandrus <davidandrus.com@gmail.com>
2017-01-15 20:46:27 -0800

    feat: using webfont loader for google font, updating gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', () => {
+gulp.task('copy', () => {
   // copy files
   gulp
     .src(['src/*', 'src/*/**', '!src/img', '!src/views/images'])
     .pipe(gulp.dest('dist'));
-
-  gulp
-    .src('src/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
-
-  gulp
-    .src('src/views/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/views/images'));
-
 });
 
-gulp.task('minify', () => {
+gulp.task('minify', ['copy'], () => {
   gulp
-    .src('src/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .src(['src/**/*.jpg','src/**/*.gif', 'src/**/*.png', 'src/**/*.svg'])
+    .pipe(imagemin())
     .pipe(gulp.dest('dist'));
 
-    
-})
+  gulp
+    .src('src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+
+  gulp
+    .src('src/**/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copy', 'minify']);
